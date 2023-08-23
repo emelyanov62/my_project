725728df4de75fa3e2770f3ce84667123320bb1b
maximka62 <114732187+maximka62@users.noreply.github.com>
2023-08-23 22:06:44 +0300

    файлы

--- финансы.py
+++ финансы.py
@@ -1,28 +1,61 @@
-import telebot
-from telebot import types
 import openpyxl
+from datetime import datetime
 
-bot = telebot.TeleBot('6616772242:AAHtXz3fkjOtdze5mG7KxmoT6vucYZewzyw')
+# Создаем новую рабочую книгу
+wb = openpyxl.Workbook()
 
-wb = openpyxl.load_workbook('C:\\Users\\User\\Desktop\\money.xlsx')
+# Выбираем активный лист (первый лист по умолчанию)
 sheet = wb.active
+sheet.title = "Финансы"
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот для учета расходов.")
+# Заголовки таблицы
+sheet['A1'] = "Дата"
+sheet['B1'] = "Описание"
+sheet['C1'] = "Доходы"
+sheet['D1'] = "Расходы"
+sheet['F1'] = "Доходы общ."
+sheet['G1'] = "Расходы общ."
 
-@bot.message_handler(commands=['статистика'])
-def get_statistics(message):
-    expenses = list(sheet.values)
+start = input('нажмите старт: ')
 
-    total = sum(int(row[0]) for row in expenses[1:])
-    bot.reply_to(message, f'Общая сумма: {total} руб')
+while start == 'старт':
+    current_date = datetime.now()
+    date = current_date.strftime('%Y-%m-%d')
+    sheet['A1'] = date
 
-@bot.message_handler(content_types=['text'])
-def add_expense(message):
-    expense = message.text.split()
-    sheet.append(expense)
-    wb.save('C:\\Users\\User\\Desktop\\money.xlsx')
-    bot.reply_to(message, 'Расход добавлен в таблицу')
+    total_income = 0
+    total_expense = 0
 
-bot.polling()
+# Ввод данных с автоматически обновляемой датой
+    while True:
+        action = input("Выберите действие (доходы/расходы/выход): ").strip().lower()
+
+        if action == 'выход':
+            break
+
+        if action not in ['доходы', 'расходы']:
+            print("Неверный выбор. Попробуйте снова.")
+            continue
+
+        time = current_date.strftime('%H:%M')
+        description = input("Введите описание: ")
+        amount = float(input("Введите сумму: "))
+
+        if action == 'доходы':
+            income = amount
+            total_income += amount
+            expense = 0
+        else:
+            income = 0
+            expense = amount
+            total_expense += expense
+
+        # Добавляем данные в таблицу
+        row_data = (time, description, income, expense, (" "), total_income, total_expense)
+        sheet.append(row_data)
+
+# Сохраняем рабочую книгу в файл
+    wb.save("C:\\Users\\User\\Desktop\\money.xlsx")
+
+# Закрываем рабочую книгу
+    wb.close()
