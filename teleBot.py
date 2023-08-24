2e25d302ab7c80e27acc6626cd6cc00432ab8355
maximka62 <114732187+maximka62@users.noreply.github.com>
2023-08-24 22:23:42 +0300

    файлы

--- teleBot.py
+++ teleBot.py
@@ -4,7 +4,7 @@ import openpyxl
 
 # Создаем новую рабочую книгу для Excel
 wb = openpyxl.Workbook()
-#токен бота
+# токен бота
 bot = telebot.TeleBot('6616772242:AAHtXz3fkjOtdze5mG7KxmoT6vucYZewzyw')
 
 # Выбираем активный лист (первый лист по умолчанию)
@@ -21,21 +21,26 @@ sheet['D1'] = "Расходы"
 # старт бота
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'привет, {message.from_user.first_name}')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}, давай запишим твой бюджет ')
 
-#доходы
+
+# доходы
 @bot.message_handler(commands=['income'])
 def income(message):
-    bot.send_message(message.chat.id, ('введите доходы'))
+    bot.send_message(message.chat.id, ('введи доход'))
 
-#расходы
+
+# расходы
 @bot.message_handler(commands=['expense'])
 def expense(message):
-    bot.send_message(message.chat.id, ('введите расходы'))
+    bot.send_message(message.chat.id, ('введи расход'))
 
-#выход
+
+# выход
 @bot.message_handler(commands=['web'])
 def web(message):
     webbrowser.open('https://www.youtube.com')
 
+
 bot.polling(non_stop=True)
+
