015c0edbd430d507e2d4d9eb4c246d8bcaccdd1e
maximka62 <114732187+maximka62@users.noreply.github.com>
2023-08-26 23:58:07 +0300

    файлы

--- погода.py
+++ погода.py
@@ -1,25 +1,23 @@
-import json
-
 import telebot
-import requests
-
+from telebot import types
 
 #токен бота
 bot = telebot.TeleBot('6616772242:AAHtXz3fkjOtdze5mG7KxmoT6vucYZewzyw')
-api = ('e819478738622a4d9741e41d934f2b01')
+
 # старт
 @bot.message_handler(commands=['start'])
-def start(sms):
-    bot.send_message(sms.chat.id, f'привет, {sms.from_user.first_name}, напиши город')
+def handle_start(message):
+    # Создаем клавиатуру
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
+    # Создаем кнопку
+    item = types.KeyboardButton("Нажми меня")
 
-#информация по городу
-@bot.message_handler(content_types = ['text'])
-def weather(sms):
-    city = sms.text.strip().lower()
-    info = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id={city}&appid={api}&units=metric&lang=ru')
-    data = json.loads(info.text)
-    bot.reply_to(sms, f'сейчас погода: {data["main"]["temp"]}')
+    # Добавляем кнопку на клавиатуру
+    markup.add(item)
+
+    # Отправляем сообщение с клавиатурой
+    bot.send_message(message.chat.id, "Привет! Я ваш телеграм-бот. Нажми кнопку:", reply_markup=markup)
 
 
 
