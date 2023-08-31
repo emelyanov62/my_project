7f8922ac3d57ae519ded64bf95c5ecf5a485665b
maximka62 <114732187+maximka62@users.noreply.github.com>
2023-08-31 22:43:13 +0300

    файлы

--- алла.py
+++ алла.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-import webbrowser
+import os
 
 bot = telebot.TeleBot('6616772242:AAHtXz3fkjOtdze5mG7KxmoT6vucYZewzyw')
 
@@ -15,43 +15,46 @@ def start(message):
                      reply_markup=button)
 
 
-@bot.callback_query_handler(lambda call: True)
-def about_me(call):
-    if call.data == 'company':
-        bot.send_message(call.message.chat.id, ('Пелёнки, пледы, комплекты из муслина 🌾 📍Рязань\n'
-                                           '•изделия из 2х слойного муслина\n'
-                                            '•ручная работа\n'
-                                           '•качество 🔝\n'
-                                            '•индивидуальный подход\n'
-                                           '•лимитированные коллекции, что делает вашу покупку уникальной\n'
-                                           '•доставка по России\n'
-                                           '•многофункциональны в применении\n'))
+@bot.callback_query_handler(lambda message: True)
+def about_me(message):
+    if message.data == 'company':
+        button = types.InlineKeyboardMarkup()
+        catalog = types.InlineKeyboardButton(text='посмотреть пеленки', callback_data='catalog')
+        button.add(catalog)
+        bot.send_message(message.message.chat.id, ('Пелёнки, пледы, комплекты из муслина 🌾 📍Рязань\n'
+                                                   '•изделия из 2х слойного муслина\n'
+                                                   '•ручная работа\n'
+                                                   '•качество 🔝\n'
+                                                   '•индивидуальный подход\n'
+                                                   '•лимитированные коллекции, что делает вашу покупку уникальной\n'
+                                                   '•доставка по России\n'
+                                                   '•многофункциональны в применении\n'))
 
-    butto = types.InlineKeyboardMarkup()
-    catalog = types.InlineKeyboardButton(text='посмотреть пеленки', callback_data='catalog')
-        #contacts = types.InlineKeyboardButton(text='контакты', callback_data='contacts')
-        #menu = types.InlineKeyboardButton(text='главное меню', callback_data='menu')
+        user_name = message.from_user.first_name
+        bot.send_message(message.message.chat.id, f'{user_name}, предлагаю ознакомится с ассортиментом',
+                         reply_markup=button)
 
-    butto.add(catalog)
-    user_name = call.from_user.first_name
-    bot.send_message(call.message.chat.id, f'{user_name}, предлагаю ознакомится с ассортиментом',
-                         reply_markup=butto)
+    elif message.data == 'catalog':
 
+        photo_folder = 'C:\\Users\\User\\Desktop\\photo'
 
-@bot.callback_query_handler(lambda call: True)
-def about(call):
-    if call.data == 'catalog':
-        bot.send_message(call.message.chat.id, ('Пелёнки, пледы, комплекты из муслина 🌾 📍Рязань\n'
-                                                '•изделия из 2х слойного муслина\n'
-                                                '•ручная работа\n'
-                                                '•качество 🔝\n'
-                                                '•индивидуальный подход\n'
-                                                '•лимитированные коллекции, что делает вашу покупку уникальной\n'
-                                                '•доставка по России\n'
-                                                '•многофункциональны в применении\n'))
-        #with open('C:\\Users\\User\\Desktop\\photo.jpg', 'rb') as photo:
-         #   bot.send_photo(call.message.chat.id, photo)
+        # Получаем список файлов в папке
+        photo_files = [f for f in os.listdir(photo_folder) if os.path.isfile(os.path.join(photo_folder, f))]
 
+        # Отправляем все фотографии из папки
+        for photo_file in photo_files:
+            photo_path = os.path.join(photo_folder, photo_file)
+            with open(photo_path, 'rb') as photo:
+                bot.send_photo(message.message.chat.id, photo)
+
+        button = types.InlineKeyboardMarkup()
+        yes = types.InlineKeyboardButton(text='да', callback_data='yes')  # обо мне
+        no = types.InlineKeyboardButton(text='нет', callback_data='ho')
+
+        button.add(yes, no)
+        user_name = message.from_user.first_name
+        bot.send_message(message.message.chat.id, f'ну как ксю? ништяк?))',
+                         reply_markup=button)
 
 
 bot.infinity_polling()
